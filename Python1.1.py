bc297d8e2dcd207c5398d7557cca5862c1a23a7b
OmerCelikel <44753206+OmerCelikel@users.noreply.github.com>
2020-07-20 18:49:49 +0300

    Add files via upload

---
+++ Python1.1.py
@@ -0,0 +1,108 @@
+print(123)
+print(98.6)
+print('Merhaba Dünya')
+print("Merhaba dünya")
+
+x = 12.2
+print(x)
+
+y = 14
+print(y)
+
+x = 100
+print(x)
+
+a = 12.5
+b =35
+
+c = a * b
+
+print(c)
+
+
+
+
+degerim = 2
+
+degerim = degerim + 2 
+
+print(degerim)
+
+
+sayimiz = 13
+
+sonucum = sayimiz % 5
+
+print(sonucum)
+
+print(2**3)
+
+
+ddd = 1 + 4 
+print(ddd)
+
+print(type(ddd))
+
+yazi = 'Adım Ömer' + ' Çelikel'
+
+print(yazi)
+
+print(type(yazi))
+
+virgüllü = 92.5
+print(type(virgüllü))
+
+sayi2 = float(90)
+print(sayi2)
+print(type(sayi2))
+
+sayi3 = '4'
+print(type(sayi3))
+
+cevirme = int(sayi3)
+print(type(cevirme))
+print('kullanıcı Adınızı giriniz')
+kullanıcıadı = input()
+print(kullanıcıadı)
+
+# Kullanıcı adı alınack
+
+kullanıcıadı2 = input('Adınızı giriniz')
+print(kullanıcıadı2)
+
+# kat Çevcirici Program 
+kat = input('Kaçıncı Kattasınız')
+
+amerikaKatı = int(kat) + 1  
+print('Amerika Katı : ' , amerikaKatı)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
